58633a4a036dff21011a0e12f649b7a0bdeaae8b
yukkkkkki <2481012441@qq.com>
2020-06-02 22:40:50 +0800

    update

--- 前端一些面试题/函数防抖和函数节流.js
+++ 前端一些面试题/函数防抖和函数节流.js
@@ -42,7 +42,7 @@ function shotCat(content) {
 // (2)一般还需要保存函数的参数(一般就是事件返回的对象)和上下文(定时器存在this隐式丢失)
 // 这里不建议通过定义一个全局变量来替代闭包保存定时器标识.
 function debounce(fun, delay = 500) {
-    // let timer = null 保存定时器
+    let timer = null // 保存定时器
     return function (args) {
         let that = this;
         let _args = args;
