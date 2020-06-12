98ec110457d2295185b9e7f5f3d4d16a8926637f
yukkkkkki <2481012441@qq.com>
2020-06-12 23:29:37 +0800

    update

--- LeetCode刷题javascript/238. 除自身以外数组的乘积.js
+++ LeetCode刷题javascript/238. 除自身以外数组的乘积.js
@@ -5,19 +5,22 @@
 // 输出: [24, 12, 8, 6]
 
 // 前缀积 * 后缀积
+// 只去维护一个数组
 var productExceptSelf = function (nums) {
   const n = nums.length;
   const res = [];
   res[0] = 1;
 
   // 左缀积
+  // 用一个数组去存这个数左边元素的积
   for (let i = 1; i < n; i++) {
     res[i] = res[i - 1] * nums[i - 1];
   }
   // console.log(res)
 
-  // 右缀积
+  // right表示右缀积
   let right = 1;
+  // 从原数组末尾开始往左找
   for (let i = n - 1; i >= 0; i--) {
     res[i] *= right; // 左边积 乘上 右边积
     right *= nums[i]; // 更新右边积
