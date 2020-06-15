3ef6110549c0813253afb8fd3a39e6904737c034
yukkkkkki <2481012441@qq.com>
2020-06-15 23:38:28 +0800

    update

--- 前端一些面试题/分页/vue分页/vue-nav.js
+++ 前端一些面试题/分页/vue分页/vue-nav.js
@@ -1,14 +1,16 @@
 (function () {
-  var tm = '<div class="page-bar">' +
-    '<ul>' +
-    '<li v-if="cur!=1"><a @click="btnClick(cur-1)">上一页</a></li>' +
-    '<li v-for="index in indexs" v-bind:class="{ active: cur == index}">' +
-    '<a v-on:click="btnClick(index)">{{ index }}</a>' +
-    '</li>' +
-    '<li v-if="cur!=all"><a @click="btnClick(cur+1)">下一页</a></li>' +
-    '<li><a>共<i>{{all}}</i>页</a></li>' +
-    '</ul>' +
-    '</div>'
+  var tm = `
+    <div class="page-bar">
+      <ul>
+        <li v-if="cur != 1"><a v-on:click="cur--">上一页</a></li>
+        <li v-for="index in indexs"  v-bind:class="{active: cur == index}">
+          <a v-on:click="btnClick(index)">{{ index }}</a>
+        </li>
+        <li v-if="cur != all"><a v-on:click="cur++">下一页</a></li>
+        <li><a>共<i>{{all}}</i>页</a></li>
+      </ul>
+    </div>
+    `
 
   var navBar = Vue.extend({
     template: tm,
@@ -29,41 +31,42 @@
         }
       }
     },
+
     computed: {
       indexs() {
-        var left = 1
-        var right = this.all
-        var ar = []
+        var left = 1;
+        var right = this.all;
+        var ar = [];
         if (this.all >= 11) {
           if (this.cur > 5 && this.cur < this.all - 4) {
-            left = this.cur - 5
-            right = this.cur + 4
+            left = this.cur - 5;
+            right = this.cur + 4;
           } else {
             if (this.cur <= 5) {
-              left = 1
-              right = 10
+              left = 1;
+              right = 10;
             } else {
-              right = this.all
-              left = this.all - 9
+              right = this.all;
+              left = this.all - 9;
             }
           }
         }
         while (left <= right) {
-          ar.push(left)
-          left++
+          ar.push(left);
+          left++;
         }
-        return ar
+        return ar;
       }
     },
+
     methods: {
       btnClick(page) {
         if (page != this.cur) {
-          this.callback(page)
+          this.callback(page);
         }
       }
     }
   })
 
   window.Vnav = navBar
-
 })()
