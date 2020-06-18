187dd412d64becf0539ae272ba2c01a618219b3f
yukkkkkki <2481012441@qq.com>
2020-06-18 17:20:41 +0800

    update

--- LeetCode刷题javascript/946. 验证栈序列.js
+++ LeetCode刷题javascript/946. 验证栈序列.js
@@ -15,9 +15,9 @@
 var validateStackSequences = function (pushed, popped) {
   let stack = [];
   let index = 0;
-  for(let i = 0; i < pushed.length; i++) {
+  for (let i = 0; i < pushed.length; i++) {
     stack.push(pushed[i]);
-    while(stack.length !== 0 && stack[stack.length - 1] === popped[index]) {
+    while (stack.length !== 0 && stack[stack.length - 1] === popped[index]) {
       stack.pop();
       index++;
     }
