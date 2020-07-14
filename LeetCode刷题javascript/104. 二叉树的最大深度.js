6026d74373177c4eb454cff799bb9bc18e2f74f8
yukkkkkki <2481012441@qq.com>
2020-07-14 23:42:22 +0800

    update

--- LeetCode刷题javascript/104. 二叉树的最大深度.js
+++ LeetCode刷题javascript/104. 二叉树的最大深度.js
@@ -15,7 +15,27 @@
 //    15   7
 
 // 返回它的最大深度 3 。
+
+// 方法一：递归
 var maxDepth = function (root) {
   if (!root) return 0;
   return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+};
+
+// 方法二：BFS
+// 通过创建FIFO队列，迭代每一层元素，每迭代一层，level+1
+var maxDepth = function (root) {
+  if (root === null) return 0;
+  let queue = [root];
+  let level = 0;
+  while (queue.length > 0) {
+    let len = queue.length;
+    while (len--) {
+      let first = queue.shift(); // 左出
+      if (first.left) queue.push(first.left);
+      if (first.right) queue.push(first.right);
+    }
+    level++;
+  }
+  return level;
 };
