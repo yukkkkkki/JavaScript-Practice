eab22480856c6895166e41a2513368cdc73d4bab
yukkkkkki <2481012441@qq.com>
2020-12-03 17:08:56 +0800

    update

--- LeetCode/204. 计数质数.js
+++ LeetCode/204. 计数质数.js
@@ -8,29 +8,18 @@
 // 方法一：暴力法
 // 根据定义直接从 2 开始直到 n 根据定义判断每一个数字是否为质数
 function isPrime(n) {
-  if (n === 2 || n === 3) {
-    return true;
-  }
-
-  if (n % 6 !== 1 && n % 6 !== 5) {
-    return false;
-  }
-
+  if (n === 2 || n === 3) return true;
+  if (n % 6 !== 1 && n % 6 !== 5) return false;
   const sqrtN = Math.sqrt(n);
   for (let i = 3; i <= sqrtN; i += 2) {
-    if (n % i === 0) {
-      return false;
-    }
+    if (n % i === 0) return false;
   }
   return true;
 }
-
 var countPrimes = function (n) {
   let count = 0;
   for (let i = 2; i < n; i++) {
-    if (isPrime(i)) {
-      count++;
-    }
+    if (isPrime(i)) count++;
   }
   return count;
 };
