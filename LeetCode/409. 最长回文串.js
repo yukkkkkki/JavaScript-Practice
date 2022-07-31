019b9d934def514ff5770bd8c63852c865f5f8a0
yukkkkkki <2481012441@qq.com>
2022-08-01 00:33:32 +0800

    LeetCode Update: 409

--- LeetCode/409. 最长回文串.js
+++ LeetCode/409. 最长回文串.js
@@ -2,4 +2,21 @@
  * @param {string} s
  * @return {number}
  */
-var longestPalindrome = function (s) {};
+var longestPalindrome = function (s) {
+  let map = new Map();
+  let arrS = s.split('');
+  let count = 0;
+
+  for (let i of arrS) {
+    if (map.has(i)) {
+      // 如果找到重复字符(可以回文)，count + 2，重新记录
+      map.delete(i);
+      count += 2;
+    } else {
+      map.set(i);
+    }
+  }
+
+  // 如果 map.size 存在(回文串长度为奇数)，增加一个中心节点，否则不增加
+  return count + (map.size ? 1 : 0);
+};
