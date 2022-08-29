82e28576683e629132ef85617560ce36c03c819f
yukkkkkki <2481012441@qq.com>
2022-08-29 22:32:25 +0800

    LeetCode Update：199

--- LeetCode/199. 二叉树的右视图.js
+++ LeetCode/199. 二叉树的右视图.js
@@ -18,10 +18,10 @@ var rightSideView = function (root) {
   const queue = [root];
 
   while (queue.length) {
-    const levelSize = queue.length;
+    const size = queue.length;
     const cur = [];
 
-    for (let i = 0; i < levelSize; i++) {
+    for (let i = 0; i < size; i++) {
       const node = queue.shift();
       cur.push(node.val);
 
@@ -29,7 +29,7 @@ var rightSideView = function (root) {
       node.right && queue.push(node.right);
     }
 
-    res.push(cur[levelSize - 1]);
+    res.push(cur[size - 1]);
   }
 
   return res;
